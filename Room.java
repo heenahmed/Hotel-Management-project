34a61727d9c60ffee2cafc3090e2c18328088cc4
heenahmed <66954825+heenahmed@users.noreply.github.com>
2020-11-01 16:26:39 +0530

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,33 +1,78 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+// import net.proteanit.sql.DbUtils;
+import java.sql.*;
 
 public class Room extends JFrame implements ActionListener{
     JTable t1;
     JButton b1,b2;
 
     Room(){
+        ImageIcon img1 = new ImageIcon(ClassLoader.getSystemResource("icons/eight.jpg"));
+        Image img2 = img1.getImage().getScaledInstance(600,600,Image.SCALE_DEFAULT);//to scale img completely inside the box
+        ImageIcon img3 = new ImageIcon(img2);
+        JLabel l1= new JLabel(img3);
+        l1.setBounds(500,0,600,600);
+        add(l1);
+
+        JLabel l2 = new JLabel("Room Number");
+        l2.setBounds(10,10,100,20);
+        add(l2);
+
+        JLabel l3 = new JLabel("Availability");
+        l3.setBounds(120,10,80,20);
+        add(l3);
+
+        JLabel l4 = new JLabel("Status");
+        l4.setBounds(230,10,80,20);
+        add(l4);
+
+        JLabel l5 = new JLabel("Price");
+        l5.setBounds(330,10,60,20);
+        add(l5);
+
+        JLabel l6 = new JLabel("Bed Type");
+        l6.setBounds(410,10,100,20);
+        add(l6);
+
         t1 = new JTable();
         t1.setBounds(0,40,500,400);
         add(t1);
 
         b1 = new JButton("Load Data");
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
         b1.addActionListener(this);
         b1.setBounds(100,460,120,30);
         add(b1);
 
         b2 = new JButton("Back");
+        b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
         b2.addActionListener(this);
         b2.setBounds(250,460,120,30);
         add(b2);
 
+        getContentPane().setBackground(Color.WHITE);
+
         setLayout(null);
-        setBounds(450,200,1000,600);
+        setBounds(450,200,1050,600);
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent ae){
         if (ae.getSource() == b1){
+            try{
+                conn c = new conn();
+                String str = "select *from room";
+
+                ResultSet rs = c.s.executeQuery(str);
+                t1 = setModel(DbUtils.resultSetToTableModel(rs));
+
+            }catch(Exception e){
+
+            }
 
         }else if (ae.getSource() == b2){
             new Reception().setVisible(true);
